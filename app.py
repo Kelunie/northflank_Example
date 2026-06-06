487bef1c126f127a554cfc4ab176bf1f20b56c75
Kelunie <caleb.cordero1997@gmail.com>
2026-06-06 17:51:48 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return {
+        "mensaje": "Proyecto integrador III",
+        "plataforma": "Northflank"
+    }
